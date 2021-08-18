07167e3b92836968b0d654011654e2b3fb436a0e
ricardowrk7 <ricardowrk7@gmail.com>
2021-08-18 13:55:31 -0400

    fix: email form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,40 +1,27 @@
+import sgMail from '@sendgrid/mail'
 
+sgMail.setApiKey(process.env.EMAIL_API_KEY);
 
-export default function (req, res) {
-    const Keys = require('../../config/keys');
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-          user: 'jessgaragedoorspataskala@gmail.com',
-          pass: 'Myfirst1'  //Keys.googlePassword,
-        },
-        secure: true,
-      })
+export default async (req, res) => {
+  const { email, name, number, message } = req.body
+  const msg = {
+    to: 'jessgaragedoorspataskala@gmail.com',
+    from: 'jessgaragedoorspataskala@gmail.com',
+    subject: `Message From ${name}`,
+    name,
+    text: `
+    Name: ${name}
+    email: ${email}
+    Phone: ${number}
+    
+    Message: ${message}
+    `,
+  };
 
-      const mailData = {
-        from: 'jessgaragedoorspataskala@gmail.com',
-        to: 'jessgaragedoorspataskala@gmail.com',
-        subject: `Message From ${req.body.name}`,
-        message: req.body.message,
-        html: `<div>
-            <p>Name: ${req.body.name}</p>
-            <p>email: ${req.body.email}</p>
-            <p>Phone: ${req.body.number}</p>
-            <p>Message: ${req.body.message}</p>
-            </div>`
-       }
-
-       transporter.sendMail(mailData, function (err, info) {
-        if(err) {
-          console.log('Error')
-          console.log(err)
-        } else {
-          console.log('success')
-          console.log(info)
-        }
-      })
-      
-      res.status(200).send('success');
-    }
+  try {
+    await sgMail.send(msg);
+    res.json({ message: `Email has been sent` })
+  } catch (error) {
+    res.status(500).json({ error: 'Error sending email' })
+  }
+}
