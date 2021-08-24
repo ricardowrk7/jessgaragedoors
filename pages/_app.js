327616a8a32e97d78cb86c571168660654401216
ricardowrk7 <ricardowrk7@gmail.com>
2021-08-24 18:38:29 -0400

    feat: service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
+
+import React, { useEffect } from 'react';
 import '../styles/globals.css';
 ///styles
 import '../styles/App.scss';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/service-worker.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+    return <MyApp />
+  }, [])
+
   return <Component {...pageProps} />
 }
 
