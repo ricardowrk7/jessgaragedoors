57f4456e977e782e49bf5e3e05e7288e929c97a2
ricardowrk7 <ricardowrk7@gmail.com>
2021-08-27 11:59:00 -0400

    fix: google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,20 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-182959161-1">
+              </script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', '${process.env.GA_TRACKING_ID}');
+          `,
+            }}
+          />
         </Head>
         <body >
           <Main />
